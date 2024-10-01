ad26cc47ed2acbcf63a787e754b67e90af2e02d0
theveryfirstegg <andely.angie@gmail.com>
2024-09-30 20:24:21 -0400

    fourth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,28 +31,6 @@ export default function App() {
       </main>
 
 
-       /*  <body className="bg-secondary" >
-
-        
-
-        <Container className="d-flex justify-content-center align-items-center flex-column" style={{ minHeight: '100vh' }}>
-            
-            <h1 className="text-center mb-3">Log In</h1>
-            <InputGroup className="mb-5">
-                <InputGroupText>email</InputGroupText>
-                <FormControl type='text' placeholder="Email" value={username} onChange={(e) => setUsername(e.target.value)}></FormControl>
-            </InputGroup>
-
-            <InputGroup className="mb-3">
-                <InputGroupText>password</InputGroupText>
-                <FormControl type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}></FormControl>
-            </InputGroup>
-
-            <Button type='submit' onClick={() => signIn('github')}> Login</Button>
-
-        </Container>
-
-        </body> */
     )
     
 
